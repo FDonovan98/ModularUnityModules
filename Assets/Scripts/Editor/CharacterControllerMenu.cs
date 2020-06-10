eb4dbb5024c59a7b81828777f8047c1453200f20
HDonovan96 <harry.bd11@gmail.com>
2020-06-10 13:54:18 +0100

    Commands can now be added to an AgentInputHandler through the window

--- Assets/Scripts/Editor/CharacterControllerMenu.cs
+++ Assets/Scripts/Editor/CharacterControllerMenu.cs
@@ -8,6 +8,8 @@ public class CharacterController : EditorWindow
     string[] passiveCommands;
     GUIStyle customStyle;
 
+    public AgentInputHandler agent;
+
     [MenuItem("Window/Character Controller")]
     public static void ShowWindow()
     {
@@ -35,12 +37,16 @@ public class CharacterController : EditorWindow
 
     void OnGUI()
     {
+        customStyle = new GUIStyle(GUI.skin.button);
+        customStyle.active.background = Texture2D.redTexture;
+
+        agent = (AgentInputHandler)EditorGUILayout.ObjectField(agent, typeof(AgentInputHandler), true);
         GUILayout.BeginHorizontal();
         GUILayout.BeginVertical();
-        ArrayToLabelList(activeCommands, "textField");
+        ArrayToLabelList(activeCommands, customStyle);
         GUILayout.EndVertical();
         GUILayout.BeginVertical();
-        ArrayToLabelList(passiveCommands, "textField");
+        ArrayToLabelList(passiveCommands, customStyle);
         GUILayout.EndVertical();
         GUILayout.EndHorizontal();
     }
@@ -49,7 +55,38 @@ public class CharacterController : EditorWindow
     {
         foreach (string element in array)
         {
-            GUILayout.Label(element, labelStyle);
+            GUILayout.Button(element, labelStyle);
         }
+    }
+    
+    void ArrayToLabelList(string[] array, GUIStyle style)
+    {
+        foreach (string element in array)
+        {
+            if (GUILayout.Button(element, style))
+            {
+                if (array == activeCommands)
+                {
+                    agent.activeCommands = AddElementToArray<ActiveCommandObject>(agent.activeCommands, (ActiveCommandObject)ScriptableObject.CreateInstance(element));
+                }
+                else
+                {
+                    agent.passiveCommands = AddElementToArray<PassiveCommandObject>(agent.passiveCommands, (PassiveCommandObject)ScriptableObject.CreateInstance(element));
+                }
+            }
+        }
+    }
+
+    static T[] AddElementToArray<T>(T[] arrayToAddTo, T elementToAdd)
+    {
+        T[] temp = arrayToAddTo;
+        arrayToAddTo = new T[arrayToAddTo.Length + 1];
+        int i;
+        for (i = 0; i < temp.Length; i++)
+        {
+            arrayToAddTo[i] = temp[i];
+        }
+        arrayToAddTo[i] = elementToAdd;
+        return arrayToAddTo;
     }
 }
